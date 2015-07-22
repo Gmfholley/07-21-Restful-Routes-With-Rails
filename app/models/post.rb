8af225cbc8783d5553c89fb300e713ef81558477
Gwendolyn Holley <gmfholley@gmail.com>
2015-07-21 20:29:03 -0500

    Adds methods to for if has voted and if can vote

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,8 +5,36 @@ class Post < ActiveRecord::Base
   
   has_and_belongs_to_many :users
   
+  # returns a Boolean if this user can vote for this post
+  #
+  # users_id - Integer representing a user's id
+  #
+  # returns a Boolean
+  def can_vote?(users_id)
+    !(users_id == self.user_id)
+  end
+  
+  # returns a Boolean indicating if this user has voted
+  #
+  # users_id - Integer of the Id of the user to check
+  #
+  # returns a Boolean
+  def has_voted?(users_id)
+    votes.select{|voter| voter.id == users_id}.length > 0 
+  end
+  
+  # returns the number of votes this post has
+  #
+  # returns an Integer
   def num_votes
     self.users.length
+  end
+  
+  # returns the array of Users who voted for the post
+  #
+  # returns an Array of Users
+  def votes
+    self.users
   end
   
 end
